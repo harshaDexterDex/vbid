068cad78be4ef17f79d22935eaf78d488de74da6
ramdesh <rasade88@gmail.com>
2015-07-27 18:45:43 +0530

    Fixed database params

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,9 @@
 <?php
 $DbHost	 = "localhost";
-$DbDatabase = "webid";
+$DbDatabase = "vbid";
 $DbUser	 = "root";
 $DbPassword = "";
 $DBPrefix	= "webid_";
-$main_path	= "C:\\Projects\\xampp\\htdocs\\WeBid\\";
+$main_path	= "J:\\xampp\\htdocs\\vbid\\";
 $MD5_PREFIX = "59b2a099025e83b4ed074168dca11264";
 ?>
