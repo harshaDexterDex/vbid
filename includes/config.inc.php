b30f98dc8bcf4b623a8d7a3321ce6cff26be8dd6
ramdesh <rasade88@gmail.com>
2015-07-27 18:49:01 +0530

    Fixed more heroku vars

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,10 @@
 <?php
+//mysql://bd8cea6f7cffc4:641818e4@us-cdbr-iron-east-02.cleardb.net/heroku_e5d8c6f02fe5737?reconnect=true
 $DbHost	 = "us-cdbr-iron-east-02.cleardb.net";
 $DbDatabase = "heroku_e5d8c6f02fe5737";
 $DbUser	 = "bd8cea6f7cffc4";
 $DbPassword = "641818e4";
 $DBPrefix	= "webid_";
-$main_path	= "/";
+$main_path	= "http://vbid.herokuapp.com/";
 $MD5_PREFIX = "59b2a099025e83b4ed074168dca11264";
 ?>
