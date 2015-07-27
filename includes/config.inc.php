96a74dcefde3817607285a13448f57c8be24ae11
ramdesh <rasade88@gmail.com>
2015-07-27 18:33:05 +0530

    Fixes for heroku

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -4,6 +4,6 @@ $DbDatabase = "heroku_e5d8c6f02fe5737";
 $DbUser	 = "bd8cea6f7cffc4";
 $DbPassword = "641818e4";
 $DBPrefix	= "webid_";
-$main_path	= "C:\\Projects\\xampp\\htdocs\\WeBid\\";
+$main_path	= "/";
 $MD5_PREFIX = "59b2a099025e83b4ed074168dca11264";
 ?>
